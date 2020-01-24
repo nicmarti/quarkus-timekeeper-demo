5a06e1cb59f1ea39c8e922e78d586d04e81d995e
Nicolas Martignole <nicolas@martignole.net>
2020-01-24 16:20:29 +0100

    Add nice layout and Bootstrap

--- src/main/java/com/lunatech/ExampleResource.java
+++ src/main/java/com/lunatech/ExampleResource.java
@@ -8,18 +8,19 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
-@Produces(MediaType.TEXT_PLAIN)
 public class ExampleResource {
     @Inject
     Template hello;
 
     @GET
+    @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "hello";
     }
 
     @GET
     @Path("/{name}")
+    @Produces(MediaType.TEXT_HTML)
     public TemplateInstance get(@PathParam("name") String name){
         return hello.data("username",name);
     }

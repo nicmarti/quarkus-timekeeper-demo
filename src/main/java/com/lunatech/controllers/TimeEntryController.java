7c10dbe10f0d1ec61ca7f05342ff8efbe59b4075
Nicolas Martignole <nicolas@touilleur-express.fr>
2020-01-25 12:32:58 +0100

    Add a detailed view for a Time Entry

--- src/main/java/com/lunatech/controllers/TimeEntryController.java
+++ src/main/java/com/lunatech/controllers/TimeEntryController.java
@@ -7,13 +7,12 @@ import io.quarkus.qute.TemplateInstance;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @Path("/times")
+@Produces(MediaType.TEXT_HTML)
 @ApplicationScoped
 public class TimeEntryController {
     @Inject
@@ -22,14 +21,27 @@ public class TimeEntryController {
     @Inject
     Template timeEntries;
 
+
+    @Inject
+    Template timeEntry;
+
     @Inject
     Engine engine;
 
     @GET
-    @Produces(MediaType.TEXT_HTML)
     public TemplateInstance list() {
         List<TimeEntry> entries = TimeEntry.listAll();
         return timeEntries.data("timeEntries", entries);
+    }
+
+    @GET
+    @Path("/{id}")
+    public TemplateInstance get(@PathParam("id") Long id){
+        TimeEntry entry = TimeEntry.findById(id);
+        if(entry == null){
+            throw new WebApplicationException("This time entry does not exist", 400);
+        }
+        return timeEntry.data("timeEntry", entry );
     }
 
 }
